ce23df2c19bc1b35a12d0591e01982a378e16f6b
aaaasif <alexasif4@gmail.com>
2021-09-21 12:22:43 +0600

    js added

--- src/App.js
+++ src/App.js
@@ -12,16 +12,34 @@ function App() {
 }
 
 function Countries() {
-  const [countries, setCountries] = useState ([]);
+  const [countries, setCountries] = useState([]);
+
   useEffect(() => {
+
     fetch('https://restcountries.eu/rest/v2/all')
-    .than(res =>)
-  })
-   return (
-     <div>
-       <h2>Practice to do make a rest countries api using by react</h2>
-     </div>
-   )
+      .then(res => res.json())
+      .then(data => setCountries(data));
+
+  }, []);
+
+  return (
+    <div>
+      <h2>Traveling around the world!!</h2>
+      <h4>Countries Available: {countries.length}</h4>
+      {
+        countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+      }
+    </div>
+  )
 }
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <p>Capital: {props.capital}</p>
+    </div>
+  )
+} 
 
 export default App;
