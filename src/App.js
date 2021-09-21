344bbf8b3eb283b93f2a7c66b62bcf5f86cf5645
aaaasif <alexasif4@gmail.com>
2021-09-21 11:58:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries></Countries>
     </div>
-  );
+  )
+}
+
+function Countries() {
+   return (
+     <div>
+       <h2>Practice to do make a rest countries api using by react</h2>
+     </div>
+   )
 }
 
 export default App;
