075505edcadb5ea4726d952d35260c53d4b0822f
aaaasif <alexasif4@gmail.com>
2021-09-22 16:40:27 +0600

    js added

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.eu/rest/v2/all')
+            .then(res => res.json())
+            .then(data => setCountries(data));
+    }, [])
+
     return (
         <div>
-            
+            <h1>Gello from Countries: {countries.length}</h1>
+            <div className="countries-container">
+                {
+                    countries.map(country => <Country
+                        key={country.capital}
+                        country={country}
+                    ></Country>)
+                }
+            </div>
         </div>
     );
 };
