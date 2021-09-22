5371ccf22b5ba3fad024ddf58fbf21ee44473a23
aaaasif <alexasif4@gmail.com>
2021-09-22 17:01:51 +0600

    js added

--- src/App.js
+++ src/App.js
@@ -1,48 +1,66 @@
-// import logo from './logo.svg';
 import './App.css';
 import Countries from './components/Countries/Countries';
 import Footer from './components/Footer/Footer';
-// import React, { useEffect, useState } from 'react'
-// import Countries from './components/Countries/Countries';
 
 function App() {
   return (
-    <div className="App">      
+    <div className="App">
       <Countries></Countries>
       <Footer></Footer>
     </div>
   );
 }
 
-// function Countries() {
-//   const [countries, setCountries] = useState([]);
 
-//   useEffect(() => {
 
-//     fetch('https://restcountries.eu/rest/v2/all')
-//       .then(res => res.json())
-//       .then(data => setCountries(data));
 
-//   }, []);
 
-//   return (
-//     <div>
-//       <h2>Traveling around the world!!</h2>
-//       <h4>Countries Available: {countries.length}</h4>
-//       {
-//         countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
-//       }
-//     </div>
-//   )
-// }
 
-// function Country(props) {
-//   return (
-//     <div>
-//       <h2>Name: {props.name}</h2>
-//       <p>Capital: {props.capital}</p>
-//     </div>
-//   )
-// } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+function Countries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(data => setCountries(data));
+
+  }, []);
+
+  return (
+    <div>
+      <h2>Traveling around the world!!</h2>
+      <h4>Countries Available: {countries.length}</h4>
+      {
+        countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <p>Capital: {props.capital}</p>
+    </div>
+  )
+} */
 
 export default App;
