14b318d00673bfa7ad1e0a0efc8d8688166dbe83
aaaasif <alexasif4@gmail.com>
2021-09-22 14:54:10 +0600

    js added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useState } from 'react'
+import Countries from './components/Countries/Countries';
+// import React, { useEffect, useState } from 'react'
+// import Countries from './components/Countries/Countries';
 
 function App() {
-  
   return (
     <div className="App">
       <Countries></Countries>
@@ -11,35 +12,35 @@ function App() {
   )
 }
 
-function Countries() {
-  const [countries, setCountries] = useState([]);
+// function Countries() {
+//   const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
+//   useEffect(() => {
 
-    fetch('https://restcountries.eu/rest/v2/all')
-      .then(res => res.json())
-      .then(data => setCountries(data));
+//     fetch('https://restcountries.eu/rest/v2/all')
+//       .then(res => res.json())
+//       .then(data => setCountries(data));
 
-  }, []);
+//   }, []);
 
-  return (
-    <div>
-      <h2>Traveling around the world!!</h2>
-      <h4>Countries Available: {countries.length}</h4>
-      {
-        countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
-      }
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h2>Traveling around the world!!</h2>
+//       <h4>Countries Available: {countries.length}</h4>
+//       {
+//         countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+//       }
+//     </div>
+//   )
+// }
 
-function Country(props) {
-  return (
-    <div>
-      <h2>Name: {props.name}</h2>
-      <p>Capital: {props.capital}</p>
-    </div>
-  )
-} 
+// function Country(props) {
+//   return (
+//     <div>
+//       <h2>Name: {props.name}</h2>
+//       <p>Capital: {props.capital}</p>
+//     </div>
+//   )
+// } 
 
-export default App;
+// export default App;
