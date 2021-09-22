2a86f8ea642c748534920f019a79e59e2d7bf7b0
aaaasif <alexasif4@gmail.com>
2021-09-22 16:41:24 +0600

    js added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import Countries from './components/Countries/Countries';
 import Footer from './components/Footer/Footer';
 // import React, { useEffect, useState } from 'react'
 // import Countries from './components/Countries/Countries';
@@ -7,7 +8,7 @@ import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">      
-      
+      <Countries></Countries>
       <Footer></Footer>
     </div>
   );
